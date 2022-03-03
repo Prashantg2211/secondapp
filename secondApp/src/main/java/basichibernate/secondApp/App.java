3b8ee96b43fcdaa66de4aa3f3fdd25892c8edf79
prashant <prashanrgrj515@gmail.com>
2022-03-03 18:43:20 +0530

    just added the project

---
+++ secondApp/src/main/java/basichibernate/secondApp/App.java
@@ -0,0 +1,59 @@
+package basichibernate.secondApp;
+
+import java.util.Scanner;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import fi.beans.UserProfile;
+
+public class App 
+{
+ 
+    public static void main(String[] args)
+	{
+		Configuration hibernateConfig = new Configuration();
+		hibernateConfig.configure("user.cfg.xml");
+	try (SessionFactory hibernateFactory = hibernateConfig.buildSessionFactory();
+				Session hibernateSession = hibernateFactory.openSession();
+				Scanner scanner = new Scanner(System.in))
+
+		{
+		    //saveUser(hibernateSession, scanner);
+
+			loadUser(hibernateSession, scanner);
+
+		} catch (HibernateException e)
+    {
+			e.printStackTrace();
+		}
+	}
+
+	static void loadUser(Session hibernateSession, Scanner scanner) {
+		System.out.println("Enter the user name whose data you want to fetch");
+		String userName = scanner.next();
+		UserProfile user = (UserProfile) hibernateSession.load(UserProfile.class, userName);
+		System.out.println(user.getName());
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+	}
+
+	static void saveUser(Session hibernateSession, Scanner scanner) {
+		System.out.println("Enter the username");
+		String userName = scanner.next();
+		System.out.println("Enter the password");
+		String password = scanner.next();
+		System.out.println("Enter the name");
+		String name = scanner.next();
+		System.out.println("Enter the email");
+		String email = scanner.next();
+		UserProfile objuser = new UserProfile(userName, password, name, email);
+		Transaction hibernatetx = hibernateSession.beginTransaction();
+		hibernateSession.save(objuser);
+		hibernatetx.commit();
+		System.out.println("Record Saved...");
+	}
+}
---
+++ secondApp/src/main/java/fi/beans/UserProfile.java
@@ -0,0 +1,51 @@
+package fi.beans;
+
+public class UserProfile {
+	String userName;
+	String password;
+	String name;
+	String email;
+
+	public UserProfile() {
+	}
+
+	public UserProfile(String userName, String password, String name, String email) {
+		this.userName = userName;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setuserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
